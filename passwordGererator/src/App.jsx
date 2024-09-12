4d94935159486f25d3e6052eb549d1b8fa05e5b0
Ashwani Kumar <itsashu268@gmail.com>
2024-09-12 13:37:08 +0530

    hover

--- passwordGererator/src/App.jsx
+++ passwordGererator/src/App.jsx
@@ -49,7 +49,7 @@ function App() {
           />
           <button 
           onClick={copyPassToClipBord}
-          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0  hover:bg-sky-700" >
             Copy
           </button>
         </div>
