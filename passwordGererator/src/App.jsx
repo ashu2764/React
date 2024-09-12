1869ffd13d1abb02f292f7d9c4bf1339909a6a37
Ashwani Kumar <itsashu268@gmail.com>
2024-09-12 13:31:19 +0530

    set password range 50

--- passwordGererator/src/App.jsx
+++ passwordGererator/src/App.jsx
@@ -58,7 +58,7 @@ function App() {
             <input
               type="range"
               min={6}
-              max={100}
+              max={50}
               value={length}
               className="cursor-pointer"
               onChange={(e) => {
